ade8fd9d7c2441cb09b2165d59a4621187d0de75
Sebrin <140252816+rinamoh@users.noreply.github.com>
2024-01-22 14:10:42 +0300

    Add project files.

---
+++ movieTickets/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace movieTickets.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
