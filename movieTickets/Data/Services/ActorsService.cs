7cd4241175d280adc27576c5e7688421ab465f33
Sebrin <140252816+rinamoh@users.noreply.github.com>
2024-01-25 22:54:19 +0300

    added the services for each controller along with their interfacce created the form for new actor added add new actor functionality along with the button added a preview functinality when the user adds a picture added model validation(http post methods) added update actor functinality

---
+++ movieTickets/Data/Services/ActorsService.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using movieTickets.Models;
+
+namespace movieTickets.Data.Services
+{
+    public class ActorsService : IActorsService
+    {
+        private readonly AppDbContext _context;
+        public ActorsService(AppDbContext context)
+        {
+            _context = context;
+        }
+        public  async Task AddAsync(Actors actors)
+        {
+          await _context.Actors.AddAsync(actors);
+           await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+           var result = await _context.Actors.FirstOrDefaultAsync(n => n.Id == id);
+            _context.Actors.Remove(result); 
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<Actors>> GetAllAsync()
+        {
+            var result = await _context.Actors.ToListAsync();
+            return result;
+        }
+
+        public async Task<Actors> GetByIdAsync(int id)
+        {
+           var result=await _context.Actors.FirstOrDefaultAsync(n=> n.Id == id);
+            return result;
+        }
+
+        public async Task<Actors> UpdateAsync(int id, Actors newActor)
+        {
+            _context.Update(newActor);
+            await _context.SaveChangesAsync();  
+            return newActor;
+        }
+    }
+}
--- movieTickets/Models/Actors.cs
+++ movieTickets/Models/Actors.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
 
 namespace movieTickets.Models
 {
@@ -8,15 +9,20 @@ namespace movieTickets.Models
         public int Id { get; set; }
 
         [Display (Name ="Picture")]
+        [Required(ErrorMessage ="Profile Picture is required")]
         public string ProfilePictureURL { get; set; }
 
         [Display(Name = "Name")]
+        [Required(ErrorMessage = "FullName is required")]
+        [StringLength(50,MinimumLength =3,ErrorMessage ="Full Name must be between 3 and 50 chars")]
         public string FullName { get; set; }
 
         [Display(Name = "Biography")]
+        [Required(ErrorMessage = "Biography is required")]
         public string Biography { get; set; }
 
         //relations
+        [ValidateNever] //added because it wasn't letting me add actors with actors movie relations when adding an actor in the form
         public List<Actor_Movie> Actors_Movies { get; set; }
     }
 }
